3e63a6b14c863cfba14a77b8abffeaa38029df3c
Naufal <naufalbhurr@gmail.com>
2023-10-19 09:08:43 +0700

    update appem

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ use App\Http\Controllers\MasyarakatController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
 Route::get('/login', [authController::class, 'login'])->name('login');
